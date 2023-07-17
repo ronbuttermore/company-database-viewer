015c2d7d356aee13691b68c7a437ecc19c25e412
Ron Buttermore <ron.buttermore@gmail.com>
2023-07-17 17:16:35 -0600

    set up db and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (id, department_name)
+VALUES (1, "ENGINEERING"), 
+       (2, "LEGAL"), 
+       (3, "FINANCE"), 
+       (4, "SALES");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 100000, 4),
+       (2, "Salesperson", 80000, 4),
+       (3, "Lead Engineer", 150000, 1),
+       (4, "Software Engineer", 120000, 1),
+       (5, "Account Manager", 160000, 3),
+       (6, "Accountant", 125000, 3),
+       (7, "Legal Team Lead", 250000, 2),
+       (8, "Lawyer", 190000, 2);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "John", "Doe", 1, NULL),
+       (2, "Mike", "Chan", 2, 1),
+       (3, "Ashley", "Rodriguez", 3, NULL),
+       (4, "Kevin", "Tupik", 4, 3),
+       (5, "Kunal", "Singh", 5, NULL),
+       (6, "Malia", "Brown", 6, 5),
+       (7, "Sarah", "Lourd", 7, NULL),
+       (8, "Tom", "Allen", 8, 7);
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    console.log('Connected to the employee_db database.')
+);
+
+db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+});
+
+db.query('SELECT * FROM role', function (err, results) {
+    console.log(results);
+});
+
+db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+});
+
+/*
 inquirer
     .prompt([
         {
@@ -9,3 +34,4 @@ inquirer
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
         }
     ])
+*/
