0d26cb72b2b1248ed57ff6ec55f3fe01d094f247
Ron Buttermore <ron.buttermore@gmail.com>
2023-07-17 18:20:08 -0600

    added functions to view db tables from command line

--- index.js
+++ index.js
@@ -2,29 +2,34 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
-    console.log('Connected to the employee_db database.')
-);
+    const db = mysql.createConnection(
+        {
+            host: 'localhost',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        console.log('Connected to the employee_db database.')
+    );
 
-db.query('SELECT * FROM department', function (err, results) {
-    console.log(results);
-});
+function viewDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+    });
+};
 
-db.query('SELECT * FROM role', function (err, results) {
-    console.log(results);
-});
+function viewRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+        console.table(results);
+    });
+};
 
-db.query('SELECT * FROM employee', function (err, results) {
-    console.log(results);
-});
+function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.table(results);
+    });
+};
 
-/*
 inquirer
     .prompt([
         {
@@ -34,4 +39,20 @@ inquirer
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
         }
     ])
-*/
+    .then((data) => {
+        if (data.mainmenu == "View all departments") {
+            viewDepartments();
+        } else if (data.mainmenu == "View all roles") {
+            viewRoles();
+        } else if (data.mainmenu == "View all employees") {
+            viewEmployees();
+        } else if (data.mainmenu == "Add a department") {
+            console.log("You have chosen to add a department");
+        } else if (data.mainmenu == "Add a role") {
+            console.log("You have chosen to add a role");
+        } else if (data.mainmenu == "Add an employee") {
+            console.log("You have chosen to add an employee");
+        } else if (data.mainmenu == "Update an employee role") {
+            console.log("You have chosen to update an employee role");
+        }
+    });
